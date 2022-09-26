423e26db819e2cb60f0e7bc18135f487eb13b3a5
Sheepbacon <72983721+sheepbacon@users.noreply.github.com>
2022-09-26 21:20:03 +0800

    Add files via upload

--- tensorflow_CUDA_availible_test.py
+++ tensorflow_CUDA_availible_test.py
@@ -7,42 +7,44 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 
-print('GPU', tf.test.is_gpu_available())
+if tf.test.is_gpu_available():
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print(tf.__version__)
+  print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+  print(tf.__version__)
 
 #test the gpu compute time
 
-tf.debugging.set_log_device_placement(True)
+  tf.debugging.set_log_device_placement(True)
 
-# Place tensors on the GPU
-npTmp = np.random.random((4096, 4096)).astype(np.float32)
-npMat1 = np.stack([npTmp,npTmp],axis=2)
-npMat2 = npMat1
-
-a = tf.constant(npMat1)
-b = tf.constant(npMat2)
-
-
-start_time = time.time()
-
-# Run on the GPU
-c = a*b
-
-print("CUDA --- %s seconds ---" % (time.time() - start_time))
-
-
-
-
-with tf.device('/cpu:0'):
-  npTmp = np.random.random((1024, 1024)).astype(np.float32)
+  # Place tensors on the GPU
+  npTmp = np.random.random((4096, 4096)).astype(np.float32)
   npMat1 = np.stack([npTmp,npTmp],axis=2)
   npMat2 = npMat1
 
-  x = tf.constant(npMat1)
-  y = tf.constant(npMat2)
-  start_time = time.time()
-  c = x*y #run on the cpu
+  a = tf.constant(npMat1)
+  b = tf.constant(npMat2)
 
-print("CPU --- %s seconds ---" % (time.time() - start_time))
+
+  start_time = time.time()
+
+  # Run on the GPU
+  c = a*b
+
+  print("CUDA --- %s seconds ---" % (time.time() - start_time))
+
+
+
+
+  with tf.device('/cpu:0'):
+    npTmp = np.random.random((1024, 1024)).astype(np.float32)
+    npMat1 = np.stack([npTmp,npTmp],axis=2)
+    npMat2 = npMat1
+
+    x = tf.constant(npMat1)
+    y = tf.constant(npMat2)
+    start_time = time.time()
+    c = x*y #run on the cpu
+
+  print("CPU --- %s seconds ---" % (time.time() - start_time))
+else:
+  print("CUDA is not availble! Check the environment setting!")
